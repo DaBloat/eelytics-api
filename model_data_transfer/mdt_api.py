1b291d4aa83730ac5a40f049e31d18fb7fbaa950
DaBloat <kurtrusselvillamor1206@gmail.com>
2026-02-19 10:53:43 +0800

    Added implementation of Redis database to multipl api workers

--- model_data_transfer/mdt_api.py
+++ model_data_transfer/mdt_api.py
@@ -1,19 +1,21 @@
 from flask import Blueprint, jsonify, request
+import redis
+import json
 
 mdt_blueprint = Blueprint("mdt_api", __name__)
 
-model_data = {
-    'size': 0,
-    'group': 'NONE' 
-}
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 @mdt_blueprint.route('/live', methods=['GET'])
 def live_data():
-    return jsonify(model_data)
+    raw_data = r.get('latest_eel')
+    if raw_data:
+        return(jsonify(json.loads(raw_data)))
+    return jsonify({"size": 0, "group": "NONE"})
 
 @mdt_blueprint.route('/update', methods=['POST'])
 def update_data():
-    global model_data
-    model_data = request.json
+    data = request.json
+    r.set('latest_eel', json.dumps(data))
     return jsonify({'status':'updated'}), 200
 
