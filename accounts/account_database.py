a241afac0cdee1e9dc80993531476c630fcb9ad6
DaBloat <kurtrusselvillamor1206@gmail.com>
2026-03-02 13:38:34 +0800

    updated account api and management structure

--- accounts/account_database.py
+++ accounts/account_database.py
@@ -1,57 +1,41 @@
 import sqlite3
-from .accounts import Account
 from flask import g
 import os
 
+class Account:
+    def __init__(self, fn, ln, suf, usr, em, pas, pfp):
+        self.first_name = fn
+        self.last_name = ln
+        self.suffix = suf
+        self.username = usr
+        self.email = em
+        self.password = pas
+        self.profile_pic = pfp
+        
+    def __str__(self):
+        return f"Hi! I'm {self.first_name} {self.last_name} {self.suffix}, you can call me {self.username} and my email is {self.email} with hashed password of {self.password}"
+
 class AccountManager:
-    def __init__(self):
-        pass
+    def __init__(self, db_path):
+        self.db_path = db_path
     
+    @property
     def connection(self):
-        pass
+        if 'db' not in g:
+            g.db = sqlite3.connect(self.db_path, timeout=5)
+            g.db.row_factory = sqlite3.Row
+            g.db.execute("PRAGMA journal_mode=WAL;")
+        return g.db
     
     def disconnect(self):
-        pass
+        db = g.pop('db', None)
+        if db is not None:
+            db.close()
     
     def create_table(self):
-        pass
-    
-    def create_account(self):
-        pass
-    
-    def read_accounts(self):
-        pass
-    
-    
-DATABASE = os.path.join('database', 'eelytics.db')
-    
-def get_db():
-    """Connect to the Database and set it to be accessible as a Dictionary
-    if it not in the flask "bucket"
-
-    Returns:
-        sq3lite.Connection: The Database connection
-    """
-    if 'db' not in g:
-        g.db = sqlite3.connect(DATABASE, timeout=5)
-        g.db.row_factory = sqlite3.Row
-        g.db.execute("PRAGMA journal_mode=WAL;") # WAL MODE
-    return g.db
-
-def close_db(e=None):
-    db = g.pop('db', None)
-    if db is not None:
-        db.close()
-        
-def create_account_table():
-    """Create table, thats it
-    
-    Returns:
-        dict: this one is for the API conditionals, response
-    """
-    db = get_db()
-    cur = db.cursor()
-    cur.execute("""
+        db = self.connection
+        cur = db.cursor()
+        cur.execute("""
                 CREATE TABLE IF NOT EXISTS
                 accounts (
                     id INTEGER PRIMARY KEY,
@@ -64,101 +48,74 @@ def create_account_table():
                     profile_pic TEXT
                 )
                 """)
-    db.commit()
-    return True
-    
-def create_account(user:Account):
-    """
-    Creates an account using the Account Object.
-    returns a response in dict
-    
-    :param user: Description
-    :type user: Account
-    :return: Description
-    :rtype: dict[str, Any]
-    """
-    db = get_db()
-    cur = db.cursor()
-    try:
-        cur.execute("""
-                    INSERT INTO accounts(username, email, password, first_name, last_name, suffix, profile_pic)
-                    VALUES (?, ?, ?, ?, ?, ?, ?)""",
-                    [user.username, user.email, user.password, user.first_name, user.last_name, user.suffix, user.profile_pic])
         db.commit()
-        return {'status':True, 'desc':'Account Created'}
-    except sqlite3.Error as e:
-        return {'status':False, 'desc': str(e)}
-        
-def read_all_accounts():
-    '''Returns a list of dictionaries for usernames'''
-    db = get_db()
-    cur = db.cursor()
-    cur.execute("""
-                SELECT id, username FROM accounts
-                """)
-    return dict(cur.fetchall())
-
-def read_profile_account():
-    pass
-
-
-def get_account_cred(username):
-    '''
-    returns a Row object(dict) of user credentials, 0 if the username is nonexistent!
+        return True
     
-    :param username: accepts the username of the account
-    '''
-    db = get_db()
-    cur = db.cursor()
-    try:
+    def create_account(self, user:Account):
+        db = self.connection
+        cur = db.cursor()
+        try:
+            cur.execute("""
+                        INSERT INTO accounts (username, email, password, first_name, last_name, suffix, profile_pic)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)""",
+                        [user.username, user.email, user.password, user.first_name, user.last_name, user.suffix, user.profile_pic])
+            db.commit()
+            return {'status': True, 'desc':'Account Created'}
+        except sqlite3.Error as e:
+            return {'status': False, 'desc': str(e)}
+    
+    def read_accounts(self):
+        db = self.connection
+        cur = db.cursor()
         cur.execute("""
-                    SELECT id, username, email, password FROM accounts WHERE username = ?
-                    """, [username])
-        return dict(cur.fetchone())
-    except TypeError as e:
-        return 0
-
-def update_account(id, update_dict:dict):
-    '''
-    Logic for updating your account in the database
+                    SELECT id, username, email FROM accounts
+                    """)
+        return dict(cur.fetchall())
     
-    :param id: Account ID given by the Database
-    :param update_dict: a Dict of things you want to update
-    :type update_dict: dict
-    '''
-    db = get_db()
-    cur = db.cursor()
-    try: 
-        updates = [f'{key} = ?' for key in update_dict.keys()]
-        clause = ", ".join(updates)
-        update_values = list(update_dict.values())
-        update_values.append(id)
-        cur.execute(f"""
-                    UPDATE accounts SET {clause}
-                    WHERE id = ?
-                    """, update_values)
-
-        if cur.rowcount == 0:
-            return {'status':False, 'desc': f'no id={id}'}
+    def get_account_cred(self, username):
+        db = self.connection
+        cur = db.cursor()
+        try:
+            cur.execute("""
+                        SELECT id, username, email, password FROM accounts WHERE username=?""",
+                        [username])
+            return dict(cur.fetchone())
+        except TypeError:            
+            return 0
         
-        db.commit()
-        return {'status':True, 'desc':'Account Updated'}
-    except sqlite3.Error as e:
-        return {'status':False, 'desc': str(e)}
-    
-def delete_account(id):
-    '''
-    deleting the account using the account's id
-    
-    :param id: Account ID given by the Database
-    '''
-    db = get_db()
-    cur = db.cursor()
-    cur.execute("""
-                DELETE FROM accounts WHERE id = ?""",
-                [id])
-    if cur.rowcount == 0:
-        return {'status':False, 'desc': f'no id={id}'}
+    def update_account(self, id, update_dict:dict):
+        db = self.connection
+        cur = db.cursor()
+        try: 
+            updates = [f'{key} = ?' for key in update_dict.keys()]
+            clause = ", ".join(updates)
+            update_values = list(update_dict.values())
+            update_values.append(id)
+            cur.execute(f"""
+                        UPDATE accounts SET {clause}
+                        WHERE id = ?
+                        """, update_values)
 
-    db.commit()
-    return {'status':True, 'desc':'Account Deleted'}
+            if cur.rowcount == 0:
+                return {'status':False, 'desc': f'no id={id}'}
+            
+            db.commit()
+            return {'status':True, 'desc':'Account Updated'}
+        except sqlite3.Error as e:
+            return {'status':False, 'desc': str(e)}
+        
+    def delete_account(self, id):
+        db = self.connection
+        cur = db.cursor()
+        try:
+            cur.execute("""
+                        DELETE FROM accounts WHERE id=?""", 
+                        [id])
+            
+            if cur.rowcount == 0:
+                return {"status": False, "desc": f"No id={id} found"}
+            
+            db.commit()
+            return {"status":True, "desc":"Account Deleted"}
+        except sqlite3.Error as e:
+            return {"status": False, 'desc': str(e)}
--- accounts/accounts_api.py
+++ accounts/accounts_api.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash 
-from . import account_database, accounts
+from .account_database import Account, AccountManager
+import os
 
 accounts_blueprint = Blueprint("accounts_api", __name__)
+acc_db_client = AccountManager(os.path.join('database', 'eelytics.db'))
 
 @accounts_blueprint.route('/login', methods=['POST'])
 def login():
@@ -11,7 +13,7 @@ def login():
     username = data.get('username')
     password = data.get('password')
     
-    user_cred = account_database.get_account_cred(username)
+    user_cred = acc_db_client.get_account_cred(username)
     
     if user_cred == 0 or not check_password_hash(user_cred['password'], password):
         return jsonify({"status": "error", "message": "Invalid Username or Password"}), 401
@@ -32,8 +34,8 @@ def signup():
     
     hashed_pass = generate_password_hash(data.get('pas'))
     
-    new_account = accounts.Account(data.get('fn'), data.get('ln'), data.get('suf'), data.get('usr'), data.get('em'), hashed_pass, 'image.png')
-    response = account_database.create_account(new_account)
+    new_account = Account(data.get('fn'), data.get('ln'), data.get('suf'), data.get('usr'), data.get('em'), hashed_pass, 'image.png')
+    response = acc_db_client.create_account(new_account)
     if not response['status']:
         return jsonify({"status":"error", "message": 'Username or Email already exists'}), 409
     else:
