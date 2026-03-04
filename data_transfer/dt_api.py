c9a777441839790cc833b86bc898305fcb01fe5d
DaBloat <kurtrusselvillamor1206@gmail.com>
2026-03-05 02:59:06 +0800

    Added Tank Fetching

--- data_transfer/dt_api.py
+++ data_transfer/dt_api.py
@@ -10,7 +10,7 @@ r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 def live_eel_data():
     raw_data = r.get('latest_eel')
     if raw_data:
-        return(jsonify(json.loads(raw_data)))
+        return jsonify(json.loads(raw_data))
     return jsonify({"size": 0, "group": "NONE"})
 
 @dt_blueprint.route('/update_eel', methods=['POST'])
@@ -19,3 +19,22 @@ def update_eel_data():
     r.set('latest_eel', json.dumps(data), ex=3)
     return jsonify({'status':'updated'}), 200
 
+@dt_blueprint.route('/live_tank', methods=['GET'])
+def live_tank_status():
+    raw_data = r.get('latest_tank_stat')
+    if raw_data:
+        return jsonify(json.loads(raw_data))
+    return jsonify({"water_level": 0, "action": 'NONE'})
+
+@dt_blueprint.route('/live_tank_options', methods=['GET'])
+def live_tank_options():
+    raw_data = r.get('latest_tank_opt')
+    if raw_data:
+        return jsonify(json.loads(raw_data))
+    return jsonify({"mode": 'NONE', "maintain": 0})
+
+@dt_blueprint.route('/update_tank_options', methods=['POST'])
+def update_tank_opts():
+    data = request.json
+    r.set('latest_tank_opt', json.dumps(data))
+    return jsonify({'status':'updated'}), 200
