efae3453fc1523e18f143f3ac8142c670138e156
DaBloat <kurtrusselvillamor1206@gmail.com>
2026-03-05 10:40:25 +0800

    MQTT Tank and Gate Simul service - plan to truncate

--- mqtt/mqtt_segregate.py
+++ mqtt/mqtt_segregate.py
@@ -7,7 +7,6 @@ r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 def on_message(client, userdata, msg):
     if msg.topic == "eelytics/tank/status":
-        # Save the live ESP32 data straight to Redis for your GET api
         r.set('latest_tank_stat', msg.payload.decode())
         print(f"[MQTT] Received Live Tank Data: {msg.payload.decode()}")
         
@@ -16,9 +15,11 @@ client.on_message = on_message
 client.connect("127.0.0.1", 1883, 60)
 client.subscribe('eelytics/tank/status')
 client.loop_start()
-TOPIC = "eelytics/servos"
+GATE_TOPIC = "eelytics/servos"
+TANK_TOPIC = "eelytics/tank/control"
 
 last_state = "NONE"
+last_state_opt = None
 
 print("Starting Eel Controller Service...")
 
@@ -34,23 +35,30 @@ while True:
 
         if current_group != last_state:
             if current_group == "NONE":
-                client.publish(TOPIC, "45,0")
+                client.publish(GATE_TOPIC, "45,0")
                 print("[MQTT] Sent: 45,0 (RESET)")
             
             elif current_group == "TABLE":
-                client.publish(TOPIC, "45,0")
+                client.publish(GATE_TOPIC, "45,0")
                 print("[MQTT] Sent: 45,0 (TABLE)")
                 
             elif current_group == "KUROKO":
-                client.publish(TOPIC, "0,0")
+                client.publish(GATE_TOPIC, "0,0")
                 print("[MQTT] Sent: 0,0 (KUROKO)")
 
             elif current_group == "ELVER":
-                client.publish(TOPIC, "45,45")
+                client.publish(GATE_TOPIC, "45,45")
                 print("[MQTT] Sent: 45,45 (ELVER)")
 
             last_state = current_group
-
+            
+        raw_data_opt = r.get('latest_tank_opt')
+        if raw_data_opt and raw_data_opt != last_state_opt:
+            client.publish(TANK_TOPIC, raw_data_opt)
+            print(f"[MQTT] Set {raw_data_opt} as new Settings")
+            
+            last_state_opt = raw_data_opt
+            
     except Exception as e:
         print(f"Error in controller loop: {e}")
         
