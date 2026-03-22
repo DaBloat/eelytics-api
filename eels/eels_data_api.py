86a93bb51a64c380622fd90fc81abfe48fadb27b
DaBloat <kurtrusselvillamor1206@gmail.com>
2026-03-22 18:58:46 +0800

    adding API funstionality in the Eels Database

---
+++ eels/eels_data_api.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, request, jsonify
+from .eels_database import EelData, EelDBManager
+import os
+
+eelsdb_blueprint = Blueprint('eels_api', __name__)
+eelsdb_manager = EelDBManager(os.path.join('database', 'eelytics.db'))
+
+@eelsdb_blueprint.route('/save_batch',  methods=['POST'])
+def save_batch():
+    data = request.get_json()
+    print(data)
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from accounts.accounts_api import accounts_blueprint
 from data_transfer.dt_api import dt_blueprint
+from eels.eels_data_api import eelsdb_blueprint
 
 
 app = Flask(__name__)
@@ -15,5 +16,6 @@ def easter_egg():
 
 app.register_blueprint(accounts_blueprint, url_prefix='/api/accounts')
 app.register_blueprint(dt_blueprint, url_prefix='/api/dt')
+app.register_blueprint(eelsdb_blueprint, url_prefix='/api/eelsdb')
 if __name__ == '__main__':
     pass
